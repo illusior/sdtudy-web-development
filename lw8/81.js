2e8c0dac56744e0064ecdca9fe34c3915d904dde
beta442 <grandoblivioniv@gmail.com>
2021-04-20 00:09:03 +0300

    81 done

--- lw8/81.js
+++ lw8/81.js
@@ -1,6 +1,11 @@
 function isPrimeNumber(n) {
-    let isPrime
+    if (Array.isArray(n)) {
+        for (let i = 0; i !== n.length; i++) {
+            if (!isInteger(n[i])) return "err array has a wrong element"
+        }
+    }
     if (!isNaN(n)) {
+        let isPrime
         for (let i = 2; i <= n; i++) {
             isPrime = true
             for (let j = 2; j < i; j++) {
@@ -11,25 +16,27 @@ function isPrimeNumber(n) {
             }
         }
         if (isPrime) {
-            if (n >= 10) {
-                return `${n} is     prime number`
-            } else {
-                return ` ${n} is     prime number`
-            }
+            if (n < 10)                 return `   ${n} is  -  prime number`
+            if (n >= 10 && n < 100)     return  `  ${n} is  -  prime number`
+            if (n >= 100 && n < 1000)   return   ` ${n} is  -  prime number`
+            if (n >= 1000 && n < 10000) return    `${n} is  -  prime number`
+
         } else {
-            if (n >= 10){
-                return `${n} is not prime number`
-            } else {
-                return ` ${n} is not prime number`
-            }             
+            if (n < 10)                 return `   ${n} is not prime number`
+            if (n >= 10 && n < 100)     return  `  ${n} is not prime number`
+            if (n >= 100 && n < 1000)   return   ` ${n} is not prime number`
+            if (n >= 1000 && n < 10000) return    `${n} is not prime number`
         }      
     } else if (Array.isArray(n)) {
         let result = ''
         for (let index in n) {
             result += `${isPrimeNumber(n[index])}\n `
         }
-        return result  
+        return result      
     } else {
         return ` ${n} - not a number`
     }    
+}
+function isInteger(num) {
+    return (num ^ 0) === num;
 }
--- lw8/82_2.js
+++ lw8/82_2.js
@@ -7,7 +7,7 @@ function calc(expression) {
         if(!isNaN(Number.parseFloat(expression[i]))) {
             digits.push(Number.parseFloat(expression[i]))
         } else if(expression[i] === "*" || expression[i] === "/" || expression[i] === "+" || expression[i] === "-" || expression[i] === "^") {
-            if(digits.length < 2) return console.log("err not enough values in input expression")            
+            if(digits.length < 2) return "err missing values"            
             firstOperand = digits.pop()
             secondOperand = digits.pop()
             if(expression[i] === "*") digits.push(firstOperand * secondOperand)
@@ -16,8 +16,15 @@ function calc(expression) {
             if(expression[i] === "-") digits.push(firstOperand - secondOperand)
             if(expression[i] === "^") digits.push(Math.pow(firstOperand, secondOperand))      
         }
-    }    
-    for(const num of digits) {
-        console.log(num)
-    }
+    } 
+    if (digits.length === 1) {
+        if (!isInteger(digits[0])) {
+            return (digits[0]).toFixed(3)
+        } else {
+            return digits[0]
+        }    
+    } else return "err missing operators"
+}    
+function isInteger(num) {
+    return (num ^ 0) === num;
 }
