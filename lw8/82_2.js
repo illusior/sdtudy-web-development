d73f03038ac7dce6f151fd11dd979989e780f5ef
beta442 <grandoblivioniv@gmail.com>
2021-04-20 23:07:56 +0300

    82_2 alpha version

--- lw8/82_2.js
+++ lw8/82_2.js
@@ -1,11 +1,12 @@
 function calc(expression) {
-    expression = expression.split(" ")
+    /* функция вычисляет выражение, записанное в польской нотации, в !expression-строке! */
+    expression = formate(expression) // expression = "*+1 2.3 4,6" -> expression = ['+', '1', '2.3', '4,6']
     const digits = []
     for(let i = expression.length; i !== -1; i--) {
         let firstOperand
         let secondOperand
-        if(!isNaN(Number.parseFloat(expression[i]))) {
-            digits.push(Number.parseFloat(expression[i]))
+        if(isInteger(Number(expression[i])) || isFloat(Number(expression[i]))) {
+            digits.push(Number(expression[i]))
         } else if(expression[i] === "*" || expression[i] === "/" || expression[i] === "+" || expression[i] === "-" || expression[i] === "^") {
             if(digits.length < 2) return "err missing values"            
             firstOperand = digits.pop()
@@ -19,7 +20,7 @@ function calc(expression) {
     } 
     if (digits.length === 1) {
         if (!isInteger(digits[0])) {
-            return (digits[0]).toFixed(3)
+            return Number.parseFloat((digits[0]).toFixed(3))
         } else {
             return digits[0]
         }    
@@ -27,4 +28,24 @@ function calc(expression) {
 }    
 function isInteger(num) {
     return (num ^ 0) === num;
+}
+function formate(expression) {
+    const operations = /[\-\+\/\*\^]/gm
+    const digits = /\d+\.\d+|\d+/gm
+    const result = []
+    let resultOfOperators = expression.match(operations)
+    let resultOfDigits = expression.match(digits)
+    for(element in resultOfOperators) {
+        result.push(resultOfOperators[element])
+    }
+    for(element in resultOfDigits) {
+        result.push(resultOfDigits[element])
+    }
+    return result
+}
+function isInteger(num) {
+    return (num ^ 0) === num;  //при поразрядовых операциях отбрасывается дробная часть, 2.7 ^ 0 === 2
+}
+function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;  //по остатку от деления
 }
