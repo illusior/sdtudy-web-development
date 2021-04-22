c20e23e20727b4a9e4f0f51bb2026dabec357154
beta442 <grandoblivioniv@gmail.com>
2021-04-22 18:18:49 +0300

    82 foramted

--- lw8/82.js
+++ lw8/82.js
@@ -1,10 +1,9 @@
-function calc(expression) {
-  
+function calc(expression) {  
   /* функция вычисляет выражение, записанное в польской нотации, в !expression-строке! */
 
   if (typeof expression !== "string" || !isValid(expression)) return "ERR! Unexpected symbols!";
   if (expression === "") return "Opps... nothing happend";
-  if (!areBracketsValid(expression)) return "ERR not enough brackets";
+  if (!areBracketsValid(expression)) return "ERR! not enough brackets";
 
   elements = prepare(expression);
   let digits = [];
@@ -17,40 +16,52 @@ function calc(expression) {
 
     } else if (isOperator(elements[i])) {
 
+      let operator = elements[i];
+
       if(digits.length < 2) {
-        return `err missing value for "${elements[i]}" operator`;
+        return `ERR! missing value for "${operator}" operator`;
       }
 
-      firstOperand = digits.pop();
-      secondOperand = digits.pop();
-      if(elements[i] === "*") digits.push(firstOperand * secondOperand);
-      if(elements[i] === "+") digits.push(firstOperand + secondOperand);
-      if(elements[i] === "-") digits.push(firstOperand - secondOperand);
-      if(elements[i] === "^") digits.push(Math.pow(firstOperand, secondOperand));
-      if(elements[i] === "/") {
-        if (secondOperand !== 0) {
-          digits.push(firstOperand / secondOperand);
-        } else {
-          return "ERR! Attemption to divide by 0!";
-        }  
-      }
+      calculating(digits, operator);
 
     }  
   }
-  if (digits.length === 1) {
-    if (isFloat(digits[0])) {
-      return Number((digits[0]).toFixed(3));
-    } else {
-      return digits[0];
-    }    
-  } else {
-    let lostDigitFirst = digits.pop();
-    let lostDigitSecond = digits.pop();
-    return `err missing operator for "${lostDigitFirst}" and "${lostDigitSecond}" values`;
-  }
+  return writeAnswer(digits);
 } 
 
+
+function isValid(expression) {
+  // проверяет expression, вернёт true, если expresion валидный
+
+  const unExpectedSymbols = /[^\d+|\+|\-|\*|\/|\^| |\(|\)|\.|\d+\.\d+]/g; //valid = ["+", "-", "*", "/", "^", "(", ")", int, float]
+  let errors = expression.match(unExpectedSymbols);
+
+  if (errors === null) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+function areBracketsValid(expression) {
+  // проверяет кол-во "(" и ")" в expression, если скобок недостаточно - ошибка
+
+  let amount = 0;
+
+  for (let i = 0; i !== expression.length; i++) {
+    if (expression[i] === "(" || expression[i] === ")") amount++;
+  }  
+  if (amount % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
 function prepare(expression) {
+  // создаёт массив elements, expression = "+ 2 -3.2" -> elements = ["+", "2", "-3.2"]
 
   const expectedValues = /\d+\.\d+|\d+|(\-\d+)|\+|\-|\*|\//g;
   let elements = expression.match(expectedValues);
@@ -64,34 +75,6 @@ function prepare(expression) {
 
 }
 
-function isValid(expression) {
-
-  const unExpectedSymbols = /[^\d+|\+|\-|\*|\/|\^| |\(|\)|\.|\d+\.\d+]/g;
-  let errors = expression.match(unExpectedSymbols);
-
-  if (errors === null) {
-    return true;
-  } else {
-    return false;
-  }
-
-}
-
-function areBracketsValid(expression) {
-  
-  let amount = 0;
-
-  for (let i = 0; i !== expression.length; i++) {
-    if (expression[i] === "(" || expression[i] === ")") amount++;
-  }  
-  if (amount % 2 === 0) {
-    return true
-  } else {
-    return false
-  }
-
-}
-
 function isInteger(num) {
     return (num ^ 0) === num;  //при поразрядовых операциях отбрасывается дробная часть, 2.7 ^ 0 === 2
 }
@@ -101,6 +84,7 @@ function isFloat(n){
 }
 
 function isOperator(element) {
+  // проверяет, является ли element оператором или нет
 
   let operators = ["+", "-", "*", "/", "^"];
   let isOperator = operators.indexOf(element) !== -1;
@@ -109,6 +93,48 @@ function isOperator(element) {
     return true;
   } else {
     return false;
+  }
+
+}
+
+function calculating(digits, operator) {
+  // считает значения для digits, в зависимости от оператора
+  
+  firstOperand = digits.pop();
+  secondOperand = digits.pop()
+
+  if(operator === "*") digits.push(firstOperand * secondOperand);
+  if(operator === "+") digits.push(firstOperand + secondOperand);
+  if(operator === "-") digits.push(firstOperand - secondOperand);
+  if(operator === "^") digits.push(Math.pow(firstOperand, secondOperand));
+  if(operator === "/") {
+    if (secondOperand !== 0) {
+      digits.push(firstOperand / secondOperand);
+    } else {
+      return "ERR! Attemption to divide by 0!";
+    }  
+  }
+
+}
+
+function writeAnswer(digits) {
+  // возвращает ответ вычислений
+
+  if (digits.length === 1) {
+
+    if (isFloat(digits[0])) {
+      return Number((digits[0]).toFixed(3));
+    } else {
+      return digits[0];
+    } 
+
+  } else {
+
+    let lostDigitFirst = digits.pop();
+    let lostDigitSecond = digits.pop();
+
+    return `err missing operator for "${lostDigitFirst}" and "${lostDigitSecond}" values`;
+
   }
 
 }
